2203a28fe7553a0ea936182ca5681bd08459dcd5
Hannes Aaltonen <aaltonen.hannes@gmail.com>
2023-09-03 16:19:54 +0300

    Fix ts errors

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -5,7 +5,7 @@ export const get = (url: string) => {
   return EitherAsync(async ({ liftEither, throwE }) => {
     try {
       const response = await axios.get(url, {
-        validateStatus: (status) => status === 200 || status === 404,
+        validateStatus: (status: number) => status === 200 || status === 404,
       });
       return liftEither(Right(response));
     } catch (error) {
